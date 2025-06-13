1f27dddc1113bfd4183add1dcd1fb31f43de379b
Muhamad <ramlahhendri123@gmail.com>
2025-06-13 16:41:01 +0700

    pembenaran jokowi

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -2,7 +2,7 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-const uploadDir = "public/groceries/";
+const uploadDir = "public/images/";
 if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
 
 const storage = multer.diskStorage({
